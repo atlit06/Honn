a178cbcfeb74f24102de4bcead70a04ad32ed5d4
steinnp <steiniell@gmail.com>
2016-09-28 23:59:01 +0000

    VideoStub

--- Assign2honn/src/main/java/is/ru/honn/rutube/domain/Video.java
+++ Assign2honn/src/main/java/is/ru/honn/rutube/domain/Video.java
@@ -1,8 +1,33 @@
 package is.ru.honn.rutube.domain;
 
+import java.util.List;
+
 /**
  * Created by steinn on 28/09/16.
  */
 public class Video {
-    int ID;
+    private int videoId;
+    private String title;
+    private String description;
+    private String src;
+    private String type;
+    private List<String> tags;
+    public Video(
+            int videoId,
+            String title,
+            String description,
+            String src,
+            String type,
+            List<String> tags
+    ) {
+        this.videoId = videoId;
+        this.title = title;
+        this.description = description;
+        this.src = src;
+        this.type = type;
+        this.tags = tags;
+    }
+    public int getVideoId() {
+        return this.videoId;
+    }
 }
