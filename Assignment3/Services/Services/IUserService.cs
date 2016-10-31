eb4843d2f1ba429be269156adbe416a81abc65aa
Janus Þór Kristjánsson <Janus@clients-pool-1.nat.ru.is>
2016-10-31 12:00:44 +0000

    some changes

---
+++ Assignment3/Services/Services/IUserService.cs
@@ -0,0 +1,11 @@
+using Assignment3.Services.DataAccess;
+using Assignment3.Models;
+
+namespace Assignment3.Services
+{
+    public interface IUserService
+    {
+        private IUserDataMapper _mapper;
+        
+    }
+}
