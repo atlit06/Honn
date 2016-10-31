f10caf89c9d9b4a92260c8a78e91ecdbe5f63c94
Janus Þór Kristjánsson <Janus@clients-pool-1.nat.ru.is>
2016-10-31 23:17:48 +0000

    fixing commit bugs

--- Assignment3/Services/Services/UserService.cs
+++ Assignment3/Services/Services/UserService.cs
@@ -32,16 +32,16 @@ namespace Assignment3.Services
         }
 
         public void addFavouriteVideo(AuthorizedUserDTO user, VideoDTO video){
-            if(!_tokenService.validateUserToken(user.username, user.accessToken)){
+            int? userId = _mapper.getUserId(user.username);
+            if(userId == null){
+                throw new InvalidParametersException("User does not exist in database");
+            }
+            if(!_tokenService.validateUserToken(user.accessToken, (int)userId)){
                 throw new InvalidParametersException("To favourite a video you must use a valid token and a valid username");
             }
             
             try
             {
-                int? userId = _mapper.getUserId(user.username);
-                if(userId == null){
-                    throw new InvalidParametersException("No Matching User");
-                }
                 _mapper.addFavourite((int)userId, video.id);
             }
             catch (Exception e)
@@ -51,13 +51,18 @@ namespace Assignment3.Services
         }
 
         public void addFriend(FriendDTO friendReq){
-            if(!_tokenService.validateUserToken(friendReq.username, friendReq.accessToken)){
+
+            int? userId = _mapper.getUserId(friendReq.username);
+            if(userId == null){
+                throw new InvalidParametersException("User does not exist in database");
+            }
+
+            if(!_tokenService.validateUserToken(friendReq.accessToken, (int)userId)){
                 throw new InvalidParametersException("To add a friend you must use a valid token and a valid username");
             }
             
             try
             {
-                int? userId = _mapper.getUserId(friendReq.username);
                 int? friendId = _mapper.getUserId(friendReq.friendUsername);
                 _mapper.addFriend((int)userId, (int)friendId);
             }
