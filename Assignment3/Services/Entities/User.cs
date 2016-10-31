8d0b1fb5efc141efdc4f119599ea3bd559591a17
steinnp <steiniell@gmail.com>
2016-10-31 12:13:36 +0000

    fix

---
+++ Assignment3/Services/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace Assignment3.Services.Entities
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string fullName { get; set; }
+    }
+}
